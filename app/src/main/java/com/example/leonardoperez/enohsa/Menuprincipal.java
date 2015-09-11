e711626345ae948fb876dd551db97f9d03abbb29
enohsaleonardoperez <enohsa.leonardo.perez@gmail.com.ar>
2015-09-11 15:41:46 -0300

    11092015

--- app/src/main/java/com/example/leonardoperez/enohsa/Menuprincipal.java
+++ app/src/main/java/com/example/leonardoperez/enohsa/Menuprincipal.java
@@ -30,9 +30,7 @@ public class Menuprincipal extends ActionBarActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         switch (id) {
             case R.id.faceEH:
